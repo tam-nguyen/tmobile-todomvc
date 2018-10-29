12cb48cd6fe3e66909e9d291c5a17207a6e0a2a9
tam-nguyen <tamquocnguyen@gmail.com>
2018-10-29 13:52:02 -0700

    initilize date calendar & priority function

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,17 +1,41 @@
 import React, { Component } from "react";
 
 class TodoItem extends Component {
+
+    constructor(){
+        super();
+        this.state={
+
+        }
+    }
+    handleChange = (e) =>{
+        e.preventDefault();
+        console.log(this.refs.date.value)
+    }
   render() {
     let todo;
     todo = this.props.todo.map((todo, key) => {
       return (
-        <ul>
-          <li key={key}>{todo}</li>
-        </ul>
+        
+          <div key={key} >
+                {todo}
+                <input
+                type="date"
+                className="input-box"
+                id="date"
+                ref="date"
+                onChange={this.handleChange.bind(this)}
+                
+              />
+              <button>Priority</button>
+            </div>
+        
       );
     });
 
-    return <div>{todo}</div>;
+    return <div>
+        {todo}
+        </div>;
   }
 }
 
